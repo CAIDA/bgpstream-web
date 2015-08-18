5f5b980dc233970101e68c6dfa26b053acf2a83c
Alistair King <alistair@caida.org>
2015-08-17 17:30:25 -0700

    Fix app_dev IP checks now that proxy is in place

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,25 +3,21 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
-
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'],
-                  array(
-                      '192.172.226.97',  // gibi
-                      '192.172.226.172', // chiara-mbp
-                      '127.0.0.1',
-                      'fe80::1',
-                      '::1'
-                  )) || php_sapi_name() === 'cli-server')
+$client_ip = isset($_SERVER['HTTP_X_REAL_IP']) ? $_SERVER['HTTP_X_REAL_IP'] :
+    @$_SERVER['REMOTE_ADDR'];
+if(!in_array($client_ip, array(
+    '192.172.226.208', // bgpstream-app-1
+    '192.172.226.78',  // rommie
+    '192.172.226.97',  // gibi
+    '127.0.0.1',
+    'fe80::1',
+    '::1'
+))
 ) {
     header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    exit($_SERVER['REMOTE_ADDR'] .
+         ' is not allowed to access this file. Check ' . basename(__FILE__) .
+         ' for more information.');
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
