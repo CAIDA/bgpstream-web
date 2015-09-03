2f401d5212ad702c4f73cf3d88e6bb38dcfdc099
Alistair King <alistair@caida.org>
2015-09-03 16:12:08 -0700

    Add stub homepage

---
+++ src/CAIDA/BGPStreamWeb/HomepageBundle/Menu/Builder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace CAIDA\BGPStreamWeb\HomepageBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class Builder
+{
+    private $factory;
+    protected $authorizationCheckerInterface;
+
+    /**
+     * @param FactoryInterface                                          $factory
+     * @param \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $authorizationCheckerInterface
+     */
+    public function __construct(FactoryInterface $factory,
+                                AuthorizationCheckerInterface $authorizationCheckerInterface)
+    {
+        $this->factory = $factory;
+        $this->authorizationCheckerInterface = $authorizationCheckerInterface;
+    }
+
+    public function topNavMenu()
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('home',
+                        array(
+                            'route' => 'caida_bgpstream_web_homepage',
+                            'routeParameters' =>
+                                array(
+                                    'page' => 'home'
+                                )
+                        )
+        );
+
+        // short circuit here if not logged in at all.
+        // anything below here WILL NOT be visible to anon visitors
+        /*
+        if(!$this->authorizationCheckerInterface->isGranted('ROLE_USER')) {
+            return $menu;
+        }
+
+        $menu->addChild('explorer',
+                        array(
+                            'route'           => 'caida_charthouse_frontend_default',
+                            'routeParameters' =>
+                                array(
+                                    'page' => 'explorer'
+                                )
+                        )
+        );
+
+        $menu->addChild('admin');
+        $menu['admin']->addChild('users',
+                                 array('route' => 'caida_charthouse_security_admin',
+                                       'routeParameters' =>
+                                           array(
+                                               'page' => 'users'
+                                           )
+                                 )
+        );
+        */
+
+        return $menu;
+    }
+}
