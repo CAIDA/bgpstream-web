82462be710462169834d349ee7707bb6d7cfcb7f
Alistair King <alistair@caida.org>
2015-10-07 08:54:02 -0700

    Add Data page

---
+++ src/CAIDA/BGPStreamWeb/HomepageBundle/Resources/public/js/data.js
@@ -0,0 +1,105 @@
+require(['jquery', 'moment'], function ($, moment) {
+
+    var MOMENT_DATE_FORMAT = 'YYYY/MM/DD HH:MM'
+
+    function buildTable(tbodyId, data) {
+        var tbody = $('#' + tbodyId);
+
+        for (var collector in data.collectors) {
+            if (data.collectors.hasOwnProperty(collector)) {
+                var info = data.collectors[collector];
+
+                var row = $('<tr></tr>').appendTo(tbody);
+
+                // how many dump types are there?
+                var dumpTypeCnt = 0;
+                for (k in info.dataTypes) if (info.dataTypes.hasOwnProperty(k)) dumpTypeCnt++;
+
+                $('<td class="vertical-center" rowspan="' + dumpTypeCnt + '"><code>' + collector + '</code></td>').appendTo(row);
+
+                var newRow = 0;
+                for (dumpType in info.dataTypes) {
+                    if (info.dataTypes.hasOwnProperty(dumpType)) {
+                        if (newRow) {
+                            row = $('<tr></tr>').appendTo(tbody);
+                        }
+
+                        var humanName = dumpType == 'ribs' ? 'RIBs' : dumpType == 'updates' ? 'Updates' : dumpType;
+
+                        $('<td>' + humanName + ' (<code>' + dumpType + '</code>)</td>').appendTo(row);
+
+                        var dumpPeriod = info.dataTypes[dumpType]['dumpPeriod'];
+                        var freqMoment = moment.duration(dumpPeriod * 1000);
+                        $('<td>' + freqMoment.humanize() + '</td>').appendTo(row);
+
+                        var fromMoment = moment.unix(info.dataTypes[dumpType]['oldestDumpTime']).utc();
+                        var fromTime = fromMoment.format(MOMENT_DATE_FORMAT);
+                        var untilMoment = moment.unix(info.dataTypes[dumpType]['latestDumpTime']).utc();
+                        var untilTime = untilMoment.format(MOMENT_DATE_FORMAT);
+
+                        var diffNow = untilMoment.diff(moment());
+                        var diffNowMoment = moment.duration(diffNow);
+                        var diffClass = diffNow*-1 < dumpPeriod*1000*2 ? 'text-success' : diffNow*-1 < dumpPeriod*1000*3 ? 'text-warning' : 'text-danger';
+                        console.log('diffNow: ' + diffNow + ' period: ' + dumpPeriod);
+
+                        $('<td>' + fromTime + ' <small>(' + fromMoment.fromNow() + '</small>)</td>').appendTo(row);
+                        $('<td>' + untilTime + ' <small class="'+diffClass+'">(' + diffNowMoment.humanize() + ' ago)</small></td>').appendTo(row);
+
+                        newRow = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    function onData(json) {
+
+        // first build the route views table
+        if (!json || !json.data || !json.data.projects) {
+            onError('Malformed metadata response');
+            return;
+        }
+        var rvData = json.data.projects.routeviews;
+        if (!rvData) {
+            onError('Missing Route Views data');
+            return;
+        }
+        buildTable('rv-tbody', rvData);
+
+        var risData = json.data.projects.ris;
+        if (!risData) {
+            onError('Missing RIS data');
+            return;
+        }
+        buildTable('ris-tbody', risData);
+
+        $('.progress').hide();
+        $('table').show();
+    }
+
+    function onError(error) {
+        // TODO: handle this
+        console.error(error);
+    }
+
+    $(function () {
+        $.ajax({
+            url: '/broker/meta/projects',
+            type: 'GET',
+            dataType: 'json',
+            timeout: 10000,
+            success: function (json) {
+                if (json.hasOwnProperty('error') && json.error) {
+                    onError(json.error);
+                    return;
+                }
+
+                onData(json);
+            },
+            error: function (xOptions, textStatus, errorThrown) {
+                if (textStatus == "abort") return;  // Call intentionally aborted
+                onError(textStatus + (errorThrown ? ' (' + errorThrown + ')' : ''));
+            }
+        });
+    });
+});
